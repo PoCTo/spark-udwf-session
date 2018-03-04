f4859d3754696591ba9a56d659aab53805adb13f
PoCTo <reinkarn@gmail.com>
2018-03-04 21:52:56 +0100

    Changed the logic
    ... to generate number of session for each user instead of random session uuid's

--- src/main/scala/com/nuvola_tech/spark/SessionUDWF.scala
+++ src/main/scala/com/nuvola_tech/spark/SessionUDWF.scala
@@ -11,28 +11,30 @@ import org.apache.spark.unsafe.types.UTF8String
 
 object MyUDWF {
   val defaultMaxSessionLengthms = 3600 * 1000
-  case class SessionUDWF(timestamp:Expression, session:Expression,
+  case class SessionUDWF(timestamp:Expression,
                          sessionWindow:Expression = Literal(defaultMaxSessionLengthms)) extends AggregateWindowFunction {
     self: Product =>
 
-    override def children: Seq[Expression] = Seq(timestamp, session)
-    override def dataType: DataType = StringType
+    override def children: Seq[Expression] = Seq(timestamp)
+    override def dataType: DataType = IntegerType
 
-    protected val zero = Literal( 0L )
+    protected val zero_long = Literal( 0L )
+    protected val zero_int = Literal( 0 )
+    protected val one_int = Literal( 1 )
     protected val nullString = Literal(null:String)
 
-    protected val curentSession = AttributeReference("currentSession", StringType, nullable = true)()
+    protected val currentSession = AttributeReference("currentSession", IntegerType, nullable = true)()
     protected val previousTs =    AttributeReference("lastTs", LongType, nullable = false)()
 
-    override val aggBufferAttributes: Seq[AttributeReference] =  curentSession  :: previousTs :: Nil
+    override val aggBufferAttributes: Seq[AttributeReference] =  currentSession  :: previousTs :: Nil
 
     protected val assignSession =  If(LessThanOrEqual(Subtract(timestamp, aggBufferAttributes(1)), sessionWindow),
       aggBufferAttributes(0), // if
-      ScalaUDF( createNewSession, StringType, children = Nil))
+      Add(aggBufferAttributes(0), one_int))
 
-    override val initialValues: Seq[Expression] =  nullString :: zero :: Nil
+    override val initialValues: Seq[Expression] =  zero_int :: zero_long :: Nil
     override val updateExpressions: Seq[Expression] =
-      If(IsNotNull(session), session, assignSession) ::
+        assignSession ::
         timestamp ::
         Nil
 
@@ -40,10 +42,8 @@ object MyUDWF {
     override def prettyName: String = "makeSession"
   }
 
-  protected val  createNewSession = () => org.apache.spark.unsafe.types.UTF8String.fromString(UUID.randomUUID().toString)
-
-  def calculateSession(ts:Column,sess:Column): Column = withExpr { SessionUDWF(ts.expr,sess.expr, Literal(defaultMaxSessionLengthms)) }
-  def calculateSession(ts:Column,sess:Column, sessionWindow:Column): Column = withExpr { SessionUDWF(ts.expr,sess.expr, sessionWindow.expr) }
+  def calculateSession(ts:Column): Column = withExpr { SessionUDWF(ts.expr, Literal(defaultMaxSessionLengthms)) }
+  def calculateSession(ts:Column, sessionWindow:Column): Column = withExpr { SessionUDWF(ts.expr, sessionWindow.expr) }
 
   private def withExpr(expr: Expression): Column = new Column(expr)
 }
